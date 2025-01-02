39575e82e36653e108262bf26cebdd711c34291e
Tusharmaji135 <135tusharmaji@gmail.com>
2025-01-02 13:35:21 +0530

    IMPLEMENTED BIN

--- 01_BASICS/TaxCalc.java
+++ 01_BASICS/TaxCalc.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class TaxCalc {
 
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter Your Income : ");
-    int income = sc.nextInt();
-    double tax;
-    if (income < 500000) {
-      tax = 0;
-    } else if (income >= 500000 && income < 1000000) {
-      tax = income * 0.2;
-    } else {
-      tax = income * 0.3;
+    try (Scanner sc = new Scanner(System.in)) {
+      System.out.print("Enter Your Income : ");
+      int income = sc.nextInt();
+      double tax;
+      if (income < 500000) {
+        tax = 0;
+      } else if (income >= 500000 && income < 1000000) {
+        tax = income * 0.2;
+      } else {
+        tax = income * 0.3;
+      }
+      System.out.println("Tax : " + tax);
+      System.out.println("Your Income : " + (income - tax));
     }
-    System.out.println("Tax : " + tax);
-    System.out.println("Your Income : " + (income - tax));
 
   }
 }
