edaf5d7a31375eb9257ddf25c5f57ec90313d03a
Tusharmaji135 <135tusharmaji@gmail.com>
2025-01-02 12:56:15 +0530

    Add SumN program to calculate the sum of integers from 0 to n

---
+++ 01_BASICS/SumN.java
@@ -0,0 +1,12 @@
+public class SumN {
+  public static void main(String[] args) {
+    int n = 5;
+    int sum = 0;
+    int i = 0;
+    while (i<=n) {
+      sum += i;
+      i++;
+    }
+    System.out.println(sum);
+  }
+}
