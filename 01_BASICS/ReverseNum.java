4f378bdf933b22d9a147eee33ec74e1d746b768b
Tusharmaji135 <135tusharmaji@gmail.com>
2025-01-02 13:20:36 +0530

    Refactor Input class to use try-with-resources for Scanner and add ReverseNum and square classes

---
+++ 01_BASICS/ReverseNum.java
@@ -0,0 +1,13 @@
+public class ReverseNum {
+  public static void main(String[] args) {
+    int n = 124578;
+    int rem;
+    String sum = "";
+    while(n>0){
+     rem = n%10;
+     sum += rem;
+     n = n/10;
+    }
+    System.out.println(sum);
+  }
+}
