c9bd0a8ed0941b64fc0374b86b959947a68997e8
Tusharmaji135 <135tusharmaji@gmail.com>
2025-01-06 14:18:21 +0530

    Add methods for subarray calculations: brute force, prefix sum, and Kadane's algorithm

--- 03_ARRAYS/SubArr.java
+++ 03_ARRAYS/SubArr.java
@@ -1,27 +1,83 @@
 public class SubArr {
+  //Brute Forse
   public static void printSubArr(int nums[]) {
+    
+    int currSum = 0;
     int tp = 0;
+    int maxSum = Integer.MIN_VALUE;
     for (int i = 0; i < nums.length; i++) {
       int start = i;
       for (int j = i; j < nums.length; j++) {
         int end = j;
-        int sum = 0;
+        currSum = 0;
         for (int k = start; k <= end; k++) { // print
-          sum += nums[k];
+          currSum += nums[k];
           System.out.print("(" + nums[k] + ") "); // Sub Array
         }
-        System.out.print("= "+sum);
+        System.out.print("= "+currSum);
+        if(currSum>maxSum){
+          maxSum = currSum;
+        }
         tp++;
         System.out.println();
       }
     }
     System.out.println("Total Pairs : " + tp);
+    System.out.println("MAX : "+maxSum);
   }
+
+  //Prefix Sum
+  public static void printSubArrPre(int nums[]) {
+    
+    int currSum = 0;
+    int tp = 0;
+    int maxSum = Integer.MIN_VALUE;
+    int prefix[] = new int[nums.length];
+
+    prefix[0] = nums[0];
+    //calculate prefix array
+    for (int i = 1; i < prefix.length; i++) {
+      prefix[i] = prefix[i-1] + nums[i];
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      int start = i;
+      for (int j = i; j < nums.length; j++) {
+        int end = j;
+        
+        currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
+        if(currSum>maxSum){
+          maxSum = currSum;
+        }
+        tp++;
+        System.out.println();
+      }
+    }
+    System.out.println("Total Pairs : " + tp);
+    System.out.println("MAX : "+maxSum);
+  }
+
+  //Kadans Algo
+  public static void printSubArrKadans(int nums[]){
+    int CS = 0;
+    int max = Integer.MIN_VALUE;
+    
+    for (int i = 0; i < nums.length; i++) {   // [-2,-3,4,-1,-2,1,5,-3]
+      CS += nums[i];
+      max = Math.max(CS, max);
+      if (CS < 0) {
+        CS = 0;
+      }
+    }
+
+    System.out.println("MAX SUM : "+max);
+  }
+
 
   public static void main(String[] args) {
     // array ka koi bhi tukra jo continuos ho usse SUBARRAYS khete hai
-    int nums[] = { 2, 4, 6, 8, 10 };
-    printSubArr(nums);
+    int nums[] = { -2,-3,4,-1,-2,1,5,-3};
+    printSubArrKadans(nums);
 
   }
 }
